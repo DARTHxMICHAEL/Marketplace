40e567ca6aa7ad17b6b3ce327a3038c9226fbaf0
DERTHxMICHAEL <maniek69000000@gmail.com>
2025-04-22 22:38:11 +0200

    Breeze authentication logic, basic customization, phone profile edit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('marketplace');
+})->middleware(['auth', 'verified'])->name('marketplace');
+
+Route::get('/my-offers', function () {
+    return view('user_offers');
+})->middleware(['auth', 'verified'])->name('my_offers');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
